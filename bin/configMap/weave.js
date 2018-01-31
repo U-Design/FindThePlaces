351df6f9ed2ddfa72b7e5b634cb3f14d96b03df4
CSL <samir.kishore@nokia.com>
2018-01-31 12:09:55 +0530

    weave js

--- bin/configMap/weave.js
+++ bin/configMap/weave.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 let ls = exec('kubectl get pod --selector=weave-scope-component=app -o jsonpath={.items..metadata.name}')
 
 ls.stdout.on('data', function (data) {
-    ls = exec(`kubectl port-forward ${data} 4041`);
+    ls = exec(`kubectl port-forward ${data} 4040`);
     console.log('stdout: ' + data);
 });
 
